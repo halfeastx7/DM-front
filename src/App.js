3ff939e8577d766c56a526cd707139082e816724
halfeastx7 <hafizizan@gmail.com>
2024-08-27 20:46:59 +0800

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import Sidebar from './Sidebar'; 
-import MainContent from './MainContent';// Path to your Sidebar component
+// import React from "react";
+// import Sidebar from "./Sidebar";
+// import MainContent from "./MainContent"; // Path to your Sidebar component
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div>
+//         <Sidebar />
+//       </div>
+//       <div>
+//         <MainContent />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import Sidebar from "./Sidebar";
+import MainContent from "./MainContent";
 
 const App = () => {
   return (
-    <div className="app">
-      <Sidebar />
-      <MainContent />
+    <div style={{ display: "flex", height: "100vh" }}>
+      <Sidebar style={{ flex: "0 0 250px" }} />
+      <MainContent style={{ flex: 1, padding: "20px" }} />
     </div>
   );
 };
 
 export default App;
-
